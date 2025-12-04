6b05e2aaab4bcca1f13a94afab4f46be4f756ec7
CHAIANE LUISA MULLER <chaiane_muller@estudante.sesisenai.org.br>
2025-12-04 16:49:18 -0300

    Login e pagina home

--- frontend/login.js
+++ frontend/login.js
@@ -1,26 +1,27 @@
-function processaFormulario(evento) {
-    evento.preventDefault();
 
-    const input_usuario = document.getElementById('usuario');
-    const input_senha = document.getElementById('senha');
+function processaFormulario(evento) {
+    evento.preventDefault()
+    const input_usuario = document.getElementById('usuario')
+    const input_senha = document.getElementById('senha')
 
     let usuario = input_usuario.value;
+
     let senha = input_senha.value;
 
-    console.log(usuario, senha);
+    console.log(usuario,senha)
 
-    if (senha !== 'Senai') {
-        console.log('A senha é inválida');
+    if (senha != 'Senai') {
+        console.log('A senha é inválida')
         const dados = {
             p_usuario: usuario,
             p_senha: senha,
-        };
-        localStorage.setItem('usuario', JSON.stringify(dados));
-    } else {
-        console.log('A senha é válida');
+        }
+        localStorage.setItem('usuario', JSON.stringify(dados))
+    }
+    else {
+        console.log('A senha é válida')
     }
 }
-
 
 
 
