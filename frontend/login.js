f06735a876679b16c184e10ec02ce6a8f7896f54
CHAIANE LUISA MULLER <chaiane_muller@estudante.sesisenai.org.br>
2026-03-06 17:07:11 -0300

    Node-Red Login

--- frontend/login.js
+++ frontend/login.js
@@ -4,9 +4,9 @@ async function processaFormulario(evento) {
     const input_usuario = document.getElementById('usuario')
     const input_senha = document.getElementById('senha')
 
-    let usuario = input_usuario.value;
+    const usuario = input_usuario.value;
 
-    let senha = input_senha.value;
+    const senha = input_senha.value;
     if(!input_usuario ||!input_senha){
         alert("Inputs não encontrados")
         return;
@@ -17,24 +17,28 @@ async function processaFormulario(evento) {
 
 
     try{
-        resposta = await fetch(`http://localhost:1880/api/usuario/autenticar`, {
+      const  resposta = await fetch(`http://localhost:1880/api/usuario/autenticar`, {
         method: "POST",
-        body: JSON.stringify({ usuario, senha })
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email: usuario, senha })
     }).then((resposta) => {
         console.log(resposta)
         if (resposta.status == 200) {
-            resposta.json()
+          return resposta.json()
         }else if(resposta.status == 401){
             alert("Usuário inválido")
         }
     }).then((usuario) => {
-        let tela = document.getElementById('loadingcontainer');
+        if(usuario){
+           let tela = document.getElementById('loadingcontainer');
         tela.style.display = "block";
         setTimeout(() => {
             tela.style.display = "none";
-           // window.location.href = "home.html";
+            window.location.href = "home.html";
         }, 3000)
-
+ 
+        }
+        
 
 
     })
