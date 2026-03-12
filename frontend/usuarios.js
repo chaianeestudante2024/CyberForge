eb340e018441fea582df220ee2f3754ae43d43e5
CHAIANE LUISA MULLER <chaiane_muller@estudante.sesisenai.org.br>
2026-03-12 17:09:21 -0300

    Listar/Deletar Usuarios

--- frontend/usuarios.js
+++ frontend/usuarios.js
@@ -1,4 +1,11 @@
 
+window.onload = () => {
+    buscaUsuarios()
+}
+
+
+
+
 function processaCadastro(evento) {
     evento.preventDefault()
     const input_nome = document.getElementById('nome')
@@ -15,7 +22,7 @@ function processaCadastro(evento) {
     let data = input_data.value;
     let tipo_usuario = input_tipo_usuario.value;
 
-     
+
 
     const dados = {
         p_nome: nome,
@@ -24,14 +31,14 @@ function processaCadastro(evento) {
         p_senha: senha,
         p_data: data,
         p_tipo_usuario: tipo_usuario,
-       
+
     }
 
-    fetch(`http://localhost:1880/api/usuario/cadastrar`,{
-        method:"POST",
-        body:JSON.stringify(dados)
+    fetch(`http://localhost:1880/api/usuario/cadastrar`, {
+        method: "POST",
+        body: JSON.stringify(dados)
     }).then(res => res.json())
-      .then(data => {
+        .then(data => {
             console.log(data)
         })
 }
@@ -41,34 +48,85 @@ function processaCadastro(evento) {
 
 
 
-function buscaUsuarios(){
+function buscaUsuarios() {
     fetch(`http://localhost:1880/api/usuario/listar`).then(res => res.json())
-      .then(data => {
+        .then(data => {
+            listarUsuarios(data)
+        })
+}
+
+function removerUsuarios(id) {
+
+    fetch(`http://localhost:1880/api/remover/usuario`, {
+        method: "DELETE",
+        body: JSON.stringify({ id})
+    }).then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+
+
+}
+
+
+
+function editarUsuarios() {
+    fetch(`http://localhost:1880/api/editar/usuario`, {
+        method: "PUT",
+        body: JSON.stringify({ "msg": "editar" })
+    }).then(res => res.json())
+        .then(data => {
             console.log(data)
         })
 }
 
-function removerUsuarios(){
 
-    fetch(`http://localhost:1880/api/remover/usuario`,{  
-         method:"DELETE",
-        body:JSON.stringify({"msg":"remover"})
-    }).then(res => res.json())
-    .then(data =>{
-        console.log(data)
-    })
+/* CRIA A LISTA, PEGA O ID DO BODY E CRIA UMA NOVA LINHA com o id*/
+
+function listarUsuarios(usuarios) {
+
+    body_table = document.getElementById('bodytable')
+
+    usuarios.forEach(usuario => {
+
+
+        let data = new Date(usuario.data_nascimento);
+
+
+        let linha = `
+            <tr>
+                <td>${usuario.nome}</td>
+                <td>${usuario.email}</td>
+                <td>${usuario.sobrenome}</td>
+                <td>${usuario.senha}</td>
+                <td>${data.toLocaleDateString('pt-br')}</td>
+                <td>${usuario.tipo_usuario}</td>
+
+                <td id="botões">
+                    <button id="Remover" onclick="removerUsuarios(${usuario.id})">Remover</button>
+                    <button id="Editar" onclick="editarUsuarios(${usuario.id})">Editar</button>
+                </td>
+
+            </tr>
+        `
+
+        body_table.innerHTML += linha;
         
 
-    }
-      
+        // let linha = body_table.insertRow();
+        // let celulanome = linha.insertCell()
+        // celulanome.innerText = usuarios.nome;
+        // let celulasobrenome = linha.insertCell()
+        // celulasobrenome.innerText = usuarios.sobrenome;
+        // let celulatipo_usuario = linha.insertCell();
+        // celulatipo_usuario.innerText = usuarios.tipo_usuario;
+
+        // let botao = document.createElement("botão")
+        // botao.innerText = "Excluir"
+        // botao.onclick = removerUsuarios;
+        // table.deleteRow(linha.rowIndex)
+
+    })
 
 
-function editarUsuarios(){
-   fetch(`http://localhost:1880/api/editar/usuario`,{
-        method:"PUT",
-        body:JSON.stringify({"msg":"editar"})
-   }).then(res => res.json())
-   .then(data =>{
-    console.log(data)
-   })
 }
